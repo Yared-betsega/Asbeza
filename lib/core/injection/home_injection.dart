6e77381ab4a39d83f61fba252accae4ae19abda2
Yared Tsegaye <yaredtsegaye120@gmail.com>
2023-09-15 22:01:48 +0300

    Change package imports to relative locations

--- lib/core/injection/home_injection.dart
+++ lib/core/injection/home_injection.dart
@@ -1,10 +1,9 @@
-import 'package:asbeza/features/home/data/datasources/home_remote_data_source.dart';
-import 'package:asbeza/features/home/data/repositories/home_repository_impl.dart';
-import 'package:asbeza/features/home/domain/repositories/home_repository.dart';
-import 'package:asbeza/features/home/domain/usecases/load_home.dart';
-import 'package:asbeza/features/home/domain/usecases/spend.dart';
-import 'package:asbeza/features/home/presentation/bloc/home_bloc/home_bloc.dart';
-
+import '../../features/home/data/datasources/home_remote_data_source.dart';
+import '../../features/home/data/repositories/home_repository_impl.dart';
+import '../../features/home/domain/repositories/home_repository.dart';
+import '../../features/home/domain/usecases/load_home.dart';
+import '../../features/home/domain/usecases/spend.dart';
+import '../../features/home/presentation/bloc/home_bloc/home_bloc.dart';
 import '../../features/home/presentation/bloc/spend_bloc/spend_bloc.dart';
 import 'injection_container.dart';
 
